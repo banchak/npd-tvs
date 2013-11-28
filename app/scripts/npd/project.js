4938f598237db31a04ad4d4537134c7f79138e3b
sathit jittanupat <jsat66@hotmail.com>
2013-11-29 00:48:08 +0700

    npd - update project version

--- app/scripts/npd/project.js
+++ app/scripts/npd/project.js
@@ -13,7 +13,7 @@
 
       this.appMenu = {
         title: 'Npd3',
-        version: '0.7.1.1 2013-11-2 jsat66@gmail.com',
+        version: '0.7.1.2 2013-11-15 jsat66@gmail.com',
         menus: [
           'Product', 'Person', 'Voucher'
         ]
